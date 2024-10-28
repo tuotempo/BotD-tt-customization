d8dcebe4c61ef9fdc6443d7b83463889be7e4f3e
h.cifci <halid.cifci@docplanner.com>
2024-10-28 15:30:56 +0100

    updated notification permission logic.

--- src/detectors/notification_permissions.ts
+++ src/detectors/notification_permissions.ts
@@ -3,7 +3,17 @@ import { BotKind, BrowserKind, ComponentDict, DetectorResponse, State } from '..
 export function detectNotificationPermissions({
   notificationPermissions,
   browserKind,
+  android,
+  userAgent
 }: ComponentDict): DetectorResponse {
+
+  if (userAgent.state === State.Success && android.state === State.Success){
+    const samsungBrowserUserAgant =  userAgent.value.indexOf("SamsungBrowser") !== -1
+    
+    if(android.value === true && samsungBrowserUserAgant){
+        return false
+    }
+  }
   if (browserKind.state !== State.Success || browserKind.value !== BrowserKind.Chrome) return false
 
   if (notificationPermissions.state === State.Success && notificationPermissions.value) {
